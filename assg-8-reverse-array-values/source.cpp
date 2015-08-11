6075cf05fa50f4f6fce16c415f66b7949d7be311
Rollie Ma <rollie.ma@gmail.com>
2015-08-11 06:53:09 -0700

    Added assignment #8

---
+++ assg-8-reverse-array-values/source.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int n;
+  int i;
+  int j;
+  int tmp;
+  int half;
+
+  cin >> n;
+
+  if (n <= 1) {
+    n = 2;
+  }
+
+  if (n >= 100) {
+    n = 99;
+  }
+
+  int data[n];
+
+  for (i = 0; i < n; i++) {
+    cin >> data[i];
+  }
+
+  half = (n % 2) ? (n - 1) / 2 : n / 2;
+
+  for (i = 0; i <= half; i++) {
+    j = n - i - 1;
+
+    tmp = data[j];
+    data[j] = data[i];
+    data[i] = tmp;
+  }
+
+  for (i = 0; i < n; i++) {
+    cout << data[i] << " ";
+  }
+
+  return 0;
+}
